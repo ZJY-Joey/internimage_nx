97493e612f3886c94fb5cd32cbf3351568e1a359
Chengrui Zhu <jewel@zju.edu.cn>
2025-11-22 15:57:06 +0800

    featrue pc2octomap

---
+++ src/depth_cloud_acc/launch/pc2octomap.launch.py
@@ -0,0 +1,40 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+import launch_ros
+from launch_ros.actions import Node, ComposableNodeContainer
+
+
+
+def generate_launch_description():
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time', default_value='True',
+        description='Use simulation time if available.'
+    )
+
+
+    octomap_node = Node(
+        package='octomap_server',
+        executable='octomap_server_node',
+        name='octomap_server',
+        parameters=[{
+            'use_sim_time': LaunchConfiguration('use_sim_time'),
+            'frame_id': 'world',
+            'resolution': 0.05,
+            'sensor_model/max_range': 5.0,
+
+        }], 
+        remappings=[('cloud_in', '/internimage/segmentation/filtered/points')],
+
+    )
+    
+    
+    ld = LaunchDescription([
+        use_sim_time_arg,
+        octomap_node,
+    ])
+    return ld
+        
+    
